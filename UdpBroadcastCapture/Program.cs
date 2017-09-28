d71172f5bd41cb9344039b7fd887c4846f999bcd
andersbor <anbo@easj.dk>
2017-09-28 13:58:13 +0200

    Add project files.

---
+++ UdpBroadcastCapture/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpBroadcastCapture
+{
+    class Program
+    {
+        // https://msdn.microsoft.com/en-us/library/tst0kwb1(v=vs.110).aspx
+        // IMPORTANT Windows firewall must be open on UDP port 7000
+        // Use the network EGV5-DMU2 to capture from the local IoT devices
+        private const int Port = 7000 ;
+        //private static readonly IPAddress IpAddress = IPAddress.Parse("192.168.5.137"); 
+        private static readonly IPAddress IpAddress = IPAddress.Any;
+        // Listen for activity on all network interfaces
+        // https://msdn.microsoft.com/en-us/library/system.net.ipaddress.ipv6any.aspx
+        static void Main()
+        {
+           // int portAnyNumber = 14593;
+            IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            using (UdpClient socket = new UdpClient(remoteEndPoint))
+            {
+                socket.Client.Bind(new IPEndPoint(IPAddress.Any, Port));
+                while (true)
+                {
+                    Console.WriteLine("Waiting for broadcast {0}", socket.Client.LocalEndPoint);
+                    byte[] datagramReceived = socket.Receive(ref remoteEndPoint);
+
+                    string message = Encoding.ASCII.GetString(datagramReceived, 0, datagramReceived.Length);
+                    Console.WriteLine("Receives {0} bytes from {1} port {2} message {3}", datagramReceived.Length,
+                        remoteEndPoint.Address, remoteEndPoint.Port, message);
+                    //Parse(message);
+                }
+            }
+        }
+
+        // To parse data from the IoT devices in the teachers room, Elisagårdsvej
+        private static void Parse(string response)
+        {
+            string[] parts = response.Split(' ');
+            foreach (string part in parts)
+            {
+                Console.WriteLine(part);
+            }
+            string temperatureLine = parts[6];
+            string temperatureStr = temperatureLine.Substring(temperatureLine.IndexOf(": ") + 2);
+            Console.WriteLine(temperatureStr);
+        }
+    }
+}
