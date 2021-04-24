e3734e4d4e1f0b47cbeb740651772f61fb070d02
Anders Børjesson <anbo@zealand.dk>
2021-04-24 12:22:56 +0200

    more comments

--- UdpBroadcastCapture/Program.cs
+++ UdpBroadcastCapture/Program.cs
@@ -9,14 +9,16 @@ namespace UdpBroadcastCapture
     {
         // https://msdn.microsoft.com/en-us/library/tst0kwb1(v=vs.110).aspx
         // IMPORTANT Windows firewall must be open on UDP port 7000
-        // Use the network EGV5-DMU2 to capture from the local IoT devices
+        // https://www.windowscentral.com/how-open-port-windows-firewall
+        // Use the network MGV-xxx to capture from local IoT devices (fake or real)
         private const int Port = 7000 ;
         //private static readonly IPAddress IpAddress = IPAddress.Parse("192.168.5.137"); 
         // Listen for activity on all network interfaces
         // https://msdn.microsoft.com/en-us/library/system.net.ipaddress.ipv6any.aspx
         static void Main()
         {
-            using (UdpClient socket = new UdpClient(new IPEndPoint(IPAddress.Any, Port)))
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, Port);
+            using (UdpClient socket = new UdpClient(ipEndPoint))
             {
                 IPEndPoint remoteEndPoint = new IPEndPoint(0, 0);
                 while (true)
@@ -32,7 +34,7 @@ namespace UdpBroadcastCapture
             }
         }
 
-        // To parse data from the IoT devices in the teachers room, Elisagårdsvej
+        // To parse data from the IoT devices (depends on the protocol)
         private static void Parse(string response)
         {
             string[] parts = response.Split(' ');
